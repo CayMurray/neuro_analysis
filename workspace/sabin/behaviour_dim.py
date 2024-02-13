df2adc2e7b88b0f8d33b08d1e1cd544e207fbb4c
Cayden Murray <cayden.murray@ucalgary.ca>
2024-02-13 17:48:56 -0600

    Behaviour quantification
    - Finished first version of behaviour_dim script. Perform KS on intra and inter group differences
    
    - Removed unnecessary code in setting.json file and changed path in files to work with container directories

--- workspace/sabin/behaviour_dim.py
+++ workspace/sabin/behaviour_dim.py
@@ -1,10 +1,12 @@
 ## MODULES ##
 
 import sys
-sys.path.append('c:/Users/cayde/OneDrive/Desktop/neuro_analysis')
+sys.path.append('/workspaces/neuro_analysis')
 import re
 
+import numpy as np
 import pandas as pd
+from scipy.stats import ks_2samp
 
 from src.data_handling import ReduceDims
 from src.visualization import BaseVisualizer
@@ -15,13 +17,13 @@ from src.visualization import BaseVisualizer
 path = 'data/sabin/Full-MOFFT-Pre.xlsx'
 reducer = ReduceDims(method='PCA')
 visualizer = BaseVisualizer()
-groups = ['Male','Female']
-exclude = ['C7','C15','C19','C20']
-exclude_pattern = '|'.join(re.escape(substring) for substring in exclude)
+include = ['C5','C6','C11','C12']
+include_pattern = '|'.join(re.escape(substring) for substring in include)
+groups = ['FD','Sated']
 
 df_list = []
 sheet_names = pd.ExcelFile(path).sheet_names
-sheet_names = [i for i in sheet_names if not re.search(exclude_pattern,i)]
+sheet_names = [i for i in sheet_names if re.search(include_pattern,i)]
 
 for (val,experiment) in enumerate(sheet_names):
     df = pd.read_excel(path,sheet_name=experiment)
@@ -39,7 +41,33 @@ data = (data-data.mean())/(data.std())
 
 components = reducer.get_components(data)
 components['labels'] = filtered_df['ID'].values
-visualizer.scatter_plot(f'{groups[0],groups[1]}',components,'PCA_1','PCA_2','labels',2)
+num_of_labels = len(set(components['labels']))
 
-    
+
+## VISUALIZE AND COMPARE DISTRIBUTIONS ##
+
+#visualizer.scatter_plot(f'{groups[0]},{groups[1]}',components,'PCA_1','PCA_2','labels',num_of_labels)
+
+intra_differences = []
+inter_differences = []
+for label in set(components['labels']):
+    intra_data = components[components['labels'].str.contains(label)].drop(['labels'],axis=1)
+    inter_data = components[~components['labels'].str.contains(label)].drop(['labels'],axis=1)
+
+    for sample_1 in range(len(intra_data)):
+        point_1 = intra_data.iloc[sample_1,:].values
+        
+        for sample_2 in range(len(intra_data)):
+            if sample_1 != sample_2:
+                point_2 = intra_data.iloc[sample_2,:].values
+                intra_diff = point_2 - point_1
+                intra_differences.append(np.sqrt(np.sum(intra_diff**2)))
+
+        for sample_3 in range(len(inter_data)):
+            point_3 = inter_data.iloc[sample_3,:].values
+            inter_diff = point_3 - point_1
+            inter_differences.append(np.sqrt(np.sum(inter_diff**2)))
+
+kstat,kpvalue = ks_2samp(intra_differences,inter_differences)
+print(f'kstat: {kstat}', f'p-value: {kpvalue}')
 
